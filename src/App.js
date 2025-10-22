12c0d677615dfa0d5b61155ef21522705b9d2207
rahafalrajabii <rahafalrajabi20.gmail.com>
2025-10-22 20:06:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useRef , useState } from 'react'; 
+
 import './App.css';
 
 function App() {
-  return (
+  const [todos, setTodos] = useState([]);
+
+  const inputRef = useRef ();
+  const  handleAddTodo = () => {
+ const text = inputRef.current.value; 
+ const newItem = {completed : false , text};
+ setTodos ( [...todos , newItem ]);
+  inputRef.current.value = "";
+  };
+ const handleItemDone =(index) => {
+const newTodos = [...todos]; /*i took a copy from the array to set new data in it */
+newTodos[index].completed = !newTodos[index].completed;
+setTodos (newTodos);
+  }
+  const handleDeleteItem = (index) => {
+const newTodos = [...todos];  /*ارجعت اخدت نسخه من الاري عشان اقدر افعل و كل مره نسخه */
+newTodos.splice (index , 1); /*عشان احذف عنصر من الاري */
+setTodos (newTodos); /*بحدث الاري */
+  }
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="todo-container">
+   <h2> To Do List </h2>
+      <ul>
+        {todos.map(({text, completed}, index) => {
+          return (
+          <div className='item'>
+           <li className ={completed ? "done" : ""} key ={index} onClick={ () => handleItemDone(index)}>
+             {text} </li>   
+            
+             <span className='trash' onClick= {() => handleDeleteItem(index)} > ❌ </span>
+ </div>
+          );
+        }
+        )}
+
+         </ul>
+      <input ref={inputRef} placeholder= "Enter item ..."/>
+        <button onClick ={handleAddTodo}> Add </button>  
+    </div>
     </div>
   );
 }
 
-export default App;
+
+export default App ;
